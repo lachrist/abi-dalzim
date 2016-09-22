32ee962cfe27f7909195f099dba93f01d42937b4
lachrist <lachrist@vub.ac.be>
2016-09-22 14:56:39 +0200

    added a bunch of reps

--- js/remarks.js
+++ js/remarks.js
@@ -1,15 +1,7 @@
 
-// Cardio //
-
-exports["high-knee"] = [
-  "Les genoux doivent monter au niveau des hanches",
-  "Bien amortir les chocs, il ne doit pas y avoir de bruit"
-];
-
-exports["butt-kicker"] = [
-  "Les talons doivent bien remonter aux fesses",
-  "Bien amortir les chocs, il ne doit pas y avoir de bruit"
-];
+////////////
+// Burpee //
+////////////
 
 exports["burpee-2"] = [
   "Bien descendre les fesses en position accroupie"
@@ -19,63 +11,86 @@ exports["burpee"] = exports["burpee-1"] = exports["burpee-pushup"] = [
   "Ne pas creuser le dos en position de pompe"
 ];
 
-exports["mountain-climbing"] = [
-  "Ne pas lever les fesses"
+/////////////////
+// Butt-Kicker //
+/////////////////
+
+exports["butt-kicker"] = [
+  "Les talons doivent bien remonter aux fesses",
+  "Bien amortir les chocs, il ne doit pas y avoir de bruit"
 ];
 
+/////////////
+// Elastic //
+/////////////
 
-// Legs //
-
-exports["squat"] = [
-  "Bien sortir les fesses en arrière pour que le dos reste droit"
+exports["elastic-butterfly"] = [
+  "Essayer de reproduire un maximum le mouvement sous l'eau",
+  "Tenir ses poignet toujours perpendiculaire au mouvement",
+  "Plier un peu les coudes au début du mouvement",
+  "Les elastiques doivent venire cogner les épaules à la fin du mouvement",
+  "Déplier les poignets à la fin du mouvement pour gagner quelques centimètres"
 ];
 
-// Elbow Planks //
+exports["elastic-left-rotator"] = exports["elastic-right-rotator"] = [
+  "Le coude ne doit pas bouger",
+  "Controler le mouvement dans le phase de retour"
+];
 
-exports["plank-front"] = [
+/////////////////
+// Elbow-Plank //
+/////////////////
+
+exports["elbow-plank-front"] = [
   "Bien contrôler les abdos pour ne pas creuser le dos",
   "Les coudes doivent être bien en dessous des épaules",
   "Ne pas lever les fesses sinon c'est tricher"
 ];
 
-exports["plank-left"] = exports["plank-right"] = [
+exports["elbow-plank-left"] = exports["plank-right"] = [
   "Bien rester de profil avec les épaules l'une au dessus de l'autre",
   "Ne pas laisser les fesses s'abaisser"
 ];
 
-exports["plank-back"] = [
+exports["elbow-plank-back"] = [
   "Bien lever les fesses et esssayer de rester le plus droit possible"
 ];
 
-// Pushup Planks //
+//////////////////
+// Flutter-Kick //
+//////////////////
 
-exports["plank-pushup"] = [
-  "Faire le gros dos et sortir les épaules vers le sol"
+exports["flutter-kick"] = [
+  "Coller le menton a la poitrine pour ne PAS creuser le dos",
+  "Mettre les mains en dessous des fesses pour ne PAS cresuer le dos",
+  "Jambes tendues jusqu'à la pointe des pieds",
+  "Ne PAS creuser le dos"
 ];
 
-exports["plank-leg-left"] = exports["plank-leg-right"] = [
-  "Ne pas monter le jambes trop haut pour eviter de lever les fesses"
+///////////////
+// High-Knee //
+///////////////
+
+exports["high-knee"] = [
+  "Les genoux doivent monter au niveau des hanches",
+  "Bien amortir les chocs, il ne doit pas y avoir de bruit"
 ];
 
-exports["plank-arm-left"] = exports["plank-arm-right"] = [
-  "Ecarter les jambes pour pouvoir rester de face",
-  "Lever le bras au moins au niveau de la tête",
-  "Le nazism, c'est mal"
+///////////
+// Lunge //
+///////////
+
+///////////////////////
+// Mountain-Climbing //
+///////////////////////
+
+exports["mountain-climbing"] = [
+  "Ne pas lever les fesses"
 ];
 
-// Walkup Planks //
-
-exports["plank-walkup-right"] = [
-  "Monter ET descendre avec le bras droit",
-  "Faire le gros dos en position bras tendu"
-];
-
-exports["plank-walkup-left"] = [
-  "Monter ET descendre avec le bras gauche",
-  "Faire le gros dos en position bras tendu"
-];
-
-// Pushups //
+////////////
+// Pushup //
+////////////
 
 exports["pushup-close"] = [
   "Toujours controler ses abos; rester gainer et ne pas creuser le dos",
@@ -90,12 +105,69 @@ exports["pushup-large"] = [
   "Si vous avez du mal, ne descendez pas jusqu'au bout"
 ];
 
-// Elastics //
+//////////////////
+// Pushup-Plank //
+//////////////////
 
-exports["elastic-butterfly"] = [
-  "Essayer de reproduire un maximum le mouvement sous l'eau",
-  "Tenir ses poignet toujours perpendiculaire au mouvement",
-  "Plier un peu les coudes au début du mouvement",
-  "Les elastiques doivent venire cogner les épaules à la fin du mouvement",
-  "Déplier les poignets à la fin du mouvement pour gagner quelques centimètres"
+exports["pushup-plank-back"] = [
+  "Remonter les fesses aux maximum"
+];
+
+exports["pushup-plank-arm-left"] = exports["pushup-plank-arm-right"] = [
+  "Ecarter les jambes pour pouvoir rester de face",
+  "Lever le bras au moins au niveau de la tête",
+  "Le nazism, c'est mal"
+];
+
+exports["pushup-plank-high"] = [
+  "Faire le gros dos et sortir les épaules vers le sol"
+];
+
+exports["pushup-large-plank-low"] = [
+  "Ne pas creuser le dos ni remonter les fesses",
+  "Les coudes collé au corps"
+];
+
+exports["pushup-plank-left"] = exports["pushup-plank-right"] = [
+  "Rester bien de profile, les épaules l'une au dessus de l'autre",
+  "Ne sortir les fesses ver l'arrière"
+];
+
+exports["pushup-plank-leg-left"] = exports["pushup-plank-leg-right"] = [
+  "Ne pas monter la jambe trop haut pour eviter de lever les fesses"
+];
+
+///////////
+// Squat //
+///////////
+
+exports["squat"] = [
+  "Bien sortir les fesses en arrière pour que le dos reste droit"
+];
+
+exports["squat-jump"] = [
+  "Toucher le sol et descendre les fesses"
+]
+
+//////////////
+// Superman //
+//////////////
+
+exports["superman-plank"] = [
+  "Garder le bras bien tendu vers l'avant",
+  "La poitrine et les genoux ne doivent pas toucher le sol"
+];
+
+////////////
+// Walkup //
+////////////
+
+exports["walkup-right"] = [
+  "Monter ET descendre avec le bras droit",
+  "Faire le gros dos en position bras tendu"
+];
+
+exports["walkup-left"] = [
+  "Monter ET descendre avec le bras gauche",
+  "Faire le gros dos en position bras tendu"
 ];
