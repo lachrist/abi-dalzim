b1b5355de0f97274d6248bd3e2a3872289aa4a57
lachrist <lachrist@vub.ac.be>
2017-01-10 18:02:32 +0100

    debugged

--- js/preload.js
+++ js/preload.js
@@ -1,28 +1,25 @@
 
 module.exports = function (set, callback) {
-  var names = [];
+  var progress = 0;
   function done () {
-    names.pop();
-    names.length || callback();
+    progress++
+    (progress === set.length) && callback();
   }
   set.forEach(function (rep) {
-    if (names.indexOf(rep.name) === -1) {
-      names.push(rep.name);
-      var extensions = ["gif", "png", "jpg"];
-      var img = new Image();
-      function preload () {
-        var ext = extensions.pop();
-        if (ext)
-          img.src = "rep/"+rep.name+"."+ext;
-        else
-          done();
-      }
-      img.onload = function () {
-        rep.src = img.src;
+    var extensions = ["gif", "png", "jpg"];
+    var img = new Image();
+    function preload () {
+      var ext = extensions.pop();
+      if (ext)
+        img.src = "rep/"+rep.name+"."+ext;
+      else
         done();
-      };
-      img.onerror = preload;
-      preload();
     }
+    img.onload = function () {
+      rep.src = img.src;
+      done();
+    };
+    img.onerror = preload;
+    preload();
   });
 };
